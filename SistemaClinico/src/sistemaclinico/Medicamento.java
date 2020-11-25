aac80e44c7cd8296d3e7afa6856d4b586b18da9a
Edwin <edwin06111998@gmail.com>
2020-11-24 19:00:49 -0500

    Se agregaron correctamente las clases Paciente, Doctor y Medicamento, junto con sus respectivos metodos.

--- SistemaClinico/src/sistemaclinico/Medicamento.java
+++ SistemaClinico/src/sistemaclinico/Medicamento.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package sistemaclinico;
-
-/**
- *
- * @author PC
- */
 public class Medicamento {
+    
+    protected String medicina;
+    protected String dosis;
+    protected String observaciones;
+    protected String unidad;
+    protected int cantidadTotal;
+
+    public Medicamento() {}
+    
+    public Medicamento(String medicina, String dosis, String observaciones, String unidad, int cantidadTotal) {
+
+        this.medicina = medicina;
+        this.dosis = dosis;
+        this.observaciones = observaciones;
+        this.unidad = unidad;
+        this.cantidadTotal = cantidadTotal;
+
+    }
     
 }
